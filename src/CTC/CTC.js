c77e99d73d5eee42b8a649ca0d84d1286ffc96ad
Joe <joegocod3@gmail.com>
2021-06-22 00:01:19 -0400

    ctc control panel styling

--- src/CTC/CTC.js
+++ src/CTC/CTC.js
@@ -1,16 +1,13 @@
-import React, { useEffect, useState, useContext } from 'react';
-import { Button } from 'react-bootstrap';
+import React, { useEffect } from 'react';
+
 import config from '../config';
 import Firebase from "firebase";
 import { SpeedContext } from '../SpeedProvider';
 import TrackView from './TrackView';
 import TrainsPanel from './TrainsPanel';
+import MainPanel from './MainPanel';
 
 function CTC() {
-
-	const [speedState] = useContext(SpeedContext);
-
-	const [ledState, setLedState] = useState(false);
 
 	if (!Firebase.apps.length) {
 		Firebase.initializeApp(config);
@@ -18,24 +15,25 @@ function CTC() {
 		Firebase.app(); // if already initialized, use that one
 	}
 
-	function getLedData() {
-		let ref = Firebase.database().ref('/LED_STATUS');
-		ref.on('value', snapshot => {
-			const state = `${snapshot.val()}`;
-			setLedState(state === "ON" ? true : false);
-		});
-	}
+	// function getLedData() {
+	// 	let ref = Firebase.database().ref('/LED_STATUS');
+	// 	ref.on('value', snapshot => {
+	// 		const state = `${snapshot.val()}`;
+	// 		setLedState(state === "ON" ? true : false);
+	// 	});
+	// }
 
-	function setLedData(newState) {
-		Firebase.database().ref('/LED_STATUS').set(newState ? "ON" : "OFF");
-	}
+	// function setLedData(newState) {
+	// 	Firebase.database().ref('/LED_STATUS').set(newState ? "ON" : "OFF");
+	// }
 
-	useEffect(() => getLedData(), []);
+	// useEffect(() => getLedData(), []);
 
 	return (
 		<div>
 			<header className="App-header">
 				<TrainsPanel />
+				<MainPanel />
 				<TrackView/>
 			</header>
 		</div>
