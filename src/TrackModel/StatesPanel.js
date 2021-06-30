713a8e6267ba7b8ebd6deb2d4b5f61323acaf2c8
reholtz <lmh150@pitt.edu>
2021-06-30 19:33:54 -0400

    Added text to Current States table

--- src/TrackModel/StatesPanel.js
+++ src/TrackModel/StatesPanel.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import SlidingPane from "react-sliding-pane";
 import { Button } from 'react-bootstrap';
+import Dropdown from 'react-bootstrap/Dropdown'
+import DropdownButton from 'react-bootstrap/DropdownButton'
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -12,11 +14,55 @@ const StatesPanel = () => {
 	return (
 		<div style={{
 			textAlign: "center",
-			background: "grey",
+			background: "#cfdfe3",
 			width: "70%",
 		}}>
-			<h1>STATES</h1>
-			<p>testtext</p>
+			<h1>CURRENT STATES</h1>
+			<div style={{
+				textAlign: "left",
+				paddingLeft: 50,
+				paddingRight: 50,
+				paddingBottom: 10,
+			}}>
+				<Container fluid>
+					<Row>
+						<Col xs={4}>
+							<h4>(BOOL) AVAILABILITY</h4>
+							<p></p>
+							<p></p>
+							<p>(BOOL) Track occupied?</p>
+							<p>(BOOL) Track under maintenance?</p>
+							<p>(BOOL) Maximum capacity?</p>
+							<p>(BOOL)
+								<DropdownButton id="dropdown-basic-button" title="Any failures?">
+								<Dropdown.Item href="#/action-1">Broken rail</Dropdown.Item>
+								<Dropdown.Item href="#/action-2">Track circuit</Dropdown.Item>
+								<Dropdown.Item href="#/action-3">Transponder/beacon</Dropdown.Item>
+								</DropdownButton>
+							</p>
+						</Col>
+						<Col>
+							<h4>TRACK ELEMENTS</h4>
+							<p></p>
+							<p></p>
+							<p>Beacon: </p>
+							<p>Signal: </p>
+							<p>Switch Position: </p>
+							<p></p>
+							<p>(BOOL) Railway Crossing</p>
+							<p>(BOOL) Track Heaters</p>
+							<p>(add indent) Current temp: </p>
+						</Col>
+						<Col>
+							<h4>PASSENGERS</h4>
+							<p></p>
+							<p></p>
+							<p>Boarding: </p>
+							<p>Departing: </p>
+						</Col>
+					</Row>
+				</Container>
+			</div>
 		</div>
 	)
 }
