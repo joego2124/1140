a546f377488f3c9a1f76474869069c8ad2c37d99
Joe <joegocod3@gmail.com>
2021-06-30 01:21:08 -0400

    WIP track renderer

--- src/CTC/TrackView.js
+++ src/CTC/TrackView.js
@@ -1,24 +1,74 @@
 import React from 'react'
 import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 import { default as blockSVG } from'./assets/block.svg';
-import trackLayout from './trackLayout';
+
+function importAll(r) {
+	let images = {};
+	r.keys().map((item, index) => { images[item.replace('./', '')] = r(item); });
+	return images;
+}
+const svgs = importAll(require.context ('./assets/blocks', true, /\.svg/));
+
+var trackLayout = require("./TrackLayout.json");
+
+const maxLength = 4000;
+const gridSize = 100;
 
 const TrackView = () => {
 
-	const maxLength = 4000;
-
 	document.body.style.overflow='hidden';
 
-	var prevBlock = null;
-	var prevPos = {x: 0, y: 0};
-	var trackBlockSVGs = trackLayout.blocks.map((v, i) => {
-		// for ()
-		return <img 
-			alt="logo" 
-			src={blockSVG} 
-			style={{position: 'absolute', top: 0, left: 0}}
+	var trackBlockSVGs = [];
+	var visitedBlockIds = [];
+
+	const traceTrack = (currBlock, currPos) => {
+
+		//add current block to list of visited blocks
+		visitedBlockIds.push(currBlock.blockId);
+
+		var blockTypeName = ""; //var for determining svg to render
+		
+		//iterate through all connected blocks
+		currBlock.connectors.forEach((nextBlockId, i) => {
+			if (nextBlockId != null) {
+
+				//get nextBlock from nextBlockId
+				const nextBlock = trackLayout.blocks.find(block => block.blockId === nextBlockId); 
+	
+				 //recursively follow connected block that isn't an visited block
+				if (!visitedBlockIds.find(id => id === nextBlock.blockId)) {
+					var dx = 0, dy = 0;
+					switch(i) {
+						case 0: dx = -50; break;
+						case 1: dy = -50; break;
+						case 2: dx = 50; break;
+						case 3: dy = 50; break;
+						default: break;
+					}
+					const nextPos = { x: currPos.x + dx, y: currPos.y + dy };
+					console.log(currBlock.blockId, nextBlock.blockId);
+					traceTrack(nextBlock, nextPos);
+				}
+			}
+			blockTypeName += (nextBlockId === null ? "0" : "1"); //inc blockTypeName
+		});
+
+		//create new svg and push to trackBlockSVGs
+		console.log(blockTypeName, currPos, currBlock.blockId);
+		const newSVG = <img 
+			src={svgs[`${blockTypeName}.svg`].default} 
+			style={{
+				position: "absolute", 
+				left: currPos.x,
+				top: currPos.y, 
+				// height: "100px",
+				// width: "100px",
+			}}
 		/>;
-	});
+		trackBlockSVGs.push(newSVG);
+	}
+
+	traceTrack(trackLayout.blocks[0], {x: 0, y: 0});
 
 	return (
 		<div style={styles.track}>
@@ -37,12 +87,12 @@ const TrackView = () => {
 					</div>
 					<svg width="100%" height="100%" xmlns="http://www.w3.org/2000/svg">
 						<defs>
-							<pattern id="smallGrid" width="8" height="8" patternUnits="userSpaceOnUse">
-								<path d="M 8 0 L 0 0 0 8" fill="none" stroke="gray" strokeWidth="0.25"/>
+							<pattern id="smallGrid" width={`${gridSize / 10}`} height={`${gridSize / 10}`} patternUnits="userSpaceOnUse">
+								<path d={`M ${gridSize / 10} 0 L 0 0 0 ${gridSize / 10}`} fill="none" stroke="gray" strokeWidth="0.25"/>
 							</pattern>
-							<pattern id="grid" width="120" height="120" patternUnits="userSpaceOnUse">
-								<rect width="120" height="120" fill="url(#smallGrid)"/>
-								<path d="M 120 0 L 0 0 0 120" fill="none" stroke="gray" strokeWidth=".5"/>
+							<pattern id="grid" width={`${gridSize}`} height={`${gridSize}`} patternUnits="userSpaceOnUse">
+								<rect width={`${gridSize}`} height={`${gridSize}`} fill="url(#smallGrid)"/>
+								<path d={`M ${gridSize} 0 L 0 0 0 ${gridSize}`} fill="none" stroke="gray" strokeWidth=".5"/>
 							</pattern>
 						</defs>
 						<rect width="100%" height="100%" fill="url(#grid)" />
