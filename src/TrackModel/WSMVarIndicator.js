243fb346b623e775d75e3b4f8e8f2737ccd907b3
reholtz <lmh150@pitt.edu>
2021-07-01 14:25:02 -0400

    Updated States panel

--- src/TrackModel/WSMVarIndicator.js
+++ src/TrackModel/WSMVarIndicator.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState, useContext } from 'react';
-import { Button, Container, Row  } from 'react-bootstrap';
 import {DatabaseGet} from '../Database';
 import { BsCircleFill } from "react-icons/bs";
 
@@ -8,7 +7,7 @@ function WSMIndicator({varName, message, parentName}) {
 	const [vari, setVari] = useState(false);
 
 	useEffect(() => {setTimeout(()=>DatabaseGet(setVari, varName, parentName), 500);}, [parentName]);
-	
+
 	return (
 		<div>
 			<BsCircleFill size="1.0em" color={vari ? "#C44242" : 'green'}/>{' '}
