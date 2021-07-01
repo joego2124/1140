5e8aad9d2db3ee12a0907e7f0c2a8ee3f861f472
Joe <joegocod3@gmail.com>
2021-07-01 16:32:05 -0400

    fuck git stash

--- src/CTC/MainPanel.js
+++ src/CTC/MainPanel.js
@@ -17,45 +17,79 @@ const MainPanel = () => {
 				onRequestClose={() => setOpen(false)}
 			>
 				<div className="controlPanelHolder">
+
 					<div className = "controlPanelSection">
 
-						<div className="controlPanelValueContainer">
-							Rt. Destination
-							<Dropdown>
-								<Dropdown.Toggle variant="outline-dark" id="dropdown-basic">
-									Station Name
-								</Dropdown.Toggle>
+					</div>
 
-								<Dropdown.Menu>
-									<Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-									<Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-									<Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-								</Dropdown.Menu>
-							</Dropdown>
-						</div>
+					<div className = "controlPanelSection">
+						<div className="controlPanelSubSection">
+							<div className="controlPanelValueContainer">
+								Rt. Destination
+								<Dropdown>
+									<Dropdown.Toggle variant="outline-dark" id="dropdown-basic">
+										Station Name
+									</Dropdown.Toggle>
 
-						<div className="controlPanelValueContainer">
-							Authority
-							<div className="controlPanelValueLabel">
-								<div>50</div>
-								<div>blocks</div>
+									<Dropdown.Menu>
+										<Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+										<Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+										<Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+									</Dropdown.Menu>
+								</Dropdown>
+							</div>
+
+							<div className="controlPanelValueContainer">
+								Authority
+								<div className="controlPanelValueUnitLabel">
+									<div>50</div>
+									<div>blocks</div>
+								</div>
+							</div>
+
+							<div className="controlPanelValueContainer">
+								Departure
+								<div className="controlPanelValueUnitLabel">
+									13:00:00
+								</div>
 							</div>
 						</div>
+						<div className="controlPanelDivider"></div>
+						<div className="controlPanelSubSection">
+							<div className="controlPanelValueContainer">
+								Rt. Destination
+								<Dropdown>
+									<Dropdown.Toggle variant="outline-dark" id="dropdown-basic">
+										Station Name
+									</Dropdown.Toggle>
 
-						<div className="controlPanelValueContainer">
-							Departure
-							<div className="controlPanelValueLabel">
-								<div>50</div>
-								<div>blocks</div>
+									<Dropdown.Menu>
+										<Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+										<Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+										<Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+									</Dropdown.Menu>
+								</Dropdown>
+							</div>
+
+							<div className="controlPanelValueContainer">
+								Authority
+								<div className="controlPanelValueUnitLabel">
+									<div>50</div>
+									<div>blocks</div>
+								</div>
+							</div>
+
+							<div className="controlPanelValueContainer">
+								Departure
+								<div className="controlPanelValueUnitLabel">
+									13:00:00
+								</div>
 							</div>
 						</div>
 
 					</div>
 					<div className = "controlPanelSection">
-						test
-					</div>
-					<div className = "controlPanelSection">
-						test
+						
 					</div>
 				</div>
 			</SlidingPane>
