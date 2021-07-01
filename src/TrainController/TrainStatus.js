e162726c1462849f3c5d3244b607db0a7d05c83f
Bentanay <55263304+Bentanay@users.noreply.github.com>
2021-07-01 12:30:21 -0400

    updating benBranch

--- src/TrainController/TrainStatus.js
+++ src/TrainController/TrainStatus.js
@@ -11,6 +11,14 @@ const TrainStatus = () => {
     const [station, setStation] = useState(false);
     const [setpointSpeed, setSetpointSpeed] = useState();
     const [temperatureControl, setTemperatureControl] = useState();
+    const [currentSpeed, setCurrentSpeed] = useState();
+    const [commandedSpeed, setCommandedSpeed] = useState();
+    const [speedLimit, setSpeedLimit] = useState();
+    const [power, setPower] = useState();
+    const [authority, setAuthority] = useState();
+    const [grade, setGrade] = useState();
+    const [intTemp, setIntTemp] = useState();
+    const [extTemp, setExtTemp] = useState();
 
     Firebase.app();
 
@@ -35,6 +43,39 @@ const TrainStatus = () => {
 		ref.on('value', snapshot => {
 			setTemperatureControl(snapshot.val());
 		});
+        ref = Firebase.database().ref('/TC/CurrentSpeed');
+		ref.on('value', snapshot => {
+			setCurrentSpeed(snapshot.val());
+		});
+        ref = Firebase.database().ref('/TM/CommandedSpeed');
+		ref.on('value', snapshot => {
+			setCommandedSpeed(snapshot.val());
+		});
+        ref = Firebase.database().ref('/TM/SpeedLimit');
+		ref.on('value', snapshot => {
+			setSpeedLimit(snapshot.val());
+		});
+        ref = Firebase.database().ref('/TC/Power');
+		ref.on('value', snapshot => {
+			setPower(snapshot.val());
+		});
+        ref = Firebase.database().ref('/TM/Authority');
+		ref.on('value', snapshot => {
+			setAuthority(snapshot.val());
+		});
+        ref = Firebase.database().ref('/TM/Grade');
+		ref.on('value', snapshot => {
+			setGrade(snapshot.val());
+		});
+        ref = Firebase.database().ref('/TC/InternalTemperature');
+		ref.on('value', snapshot => {
+			setIntTemp(snapshot.val());
+		});
+        ref = Firebase.database().ref('/TC/ExternalTemperature');
+		ref.on('value', snapshot => {
+			setExtTemp(snapshot.val());
+		});
+
 	}
 
 	function setData(newState, commandType) {
@@ -99,6 +140,30 @@ const TrainStatus = () => {
 
     return (
         <div>
+            <div>
+                Current Speed: {currentSpeed}
+            </div>
+            <div>
+                Commanded Speed: {commandedSpeed}
+            </div>
+            <div>
+                Speed Limit: {speedLimit}
+            </div>
+            <div>
+                Power: {power}
+            </div>
+            <div>
+                Authority: {authority}
+            </div>
+            <div>
+                Grade: {grade}
+            </div>
+            <div>
+                Internal Temperature: {intTemp}
+            </div>
+            <div>
+                External Temperature: {extTemp}
+            </div>
             <Button 
                 variant={intLights ? "primary" : "outline-primary"}
                 onClick={() => setData(!intLights, "int")}
