69686c19c720eaa6ac44b002588ebfea403d64ed
Joe <joegocod3@gmail.com>
2021-07-01 17:42:53 -0400

    bare minimum ctc wireframe

--- src/CTC/CTC.js
+++ src/CTC/CTC.js
@@ -1,41 +1,26 @@
-import React, { useEffect } from 'react';
+import React, { useState } from 'react';
 
-import config from '../config';
-import Firebase from "firebase";
-import { SpeedContext } from '../SpeedProvider';
 import TrackView from './TrackView';
 import TrainsPanel from './TrainsPanel';
 import MainPanel from './MainPanel';
+import ScheduleModal from './ScheduleModal';
+
 
 function CTC() {
 
-	if (!Firebase.apps.length) {
-		Firebase.initializeApp(config);
-	}else {
-		Firebase.app(); // if already initialized, use that one
-	}
-
-	// function getLedData() {
-	// 	let ref = Firebase.database().ref('/LED_STATUS');
-	// 	ref.on('value', snapshot => {
-	// 		const state = `${snapshot.val()}`;
-	// 		setLedState(state === "ON" ? true : false);
-	// 	});
-	// }
-
-	// function setLedData(newState) {
-	// 	Firebase.database().ref('/LED_STATUS').set(newState ? "ON" : "OFF");
-	// }
-
-	// useEffect(() => getLedData(), []);
+	const [modalShow, setModalShow] = useState(false);
 
 	return (
 		<div>
 			<header className="App-header">
 				<TrainsPanel />
-				<MainPanel />
+				<MainPanel setModalShow={setModalShow}/>
 				<TrackView/>
 			</header>
+			<ScheduleModal
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+      />
 		</div>
 	)
 }
