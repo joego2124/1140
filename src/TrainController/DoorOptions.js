3b32caa063b0e27e080f3154cddccf7f982a9df2
Bentanay <55263304+Bentanay@users.noreply.github.com>
2021-07-01 14:52:25 -0400

    c

--- src/TrainController/DoorOptions.js
+++ src/TrainController/DoorOptions.js
@@ -1,6 +1,15 @@
 import React, { useState, useEffect, useCallback} from 'react'
 import { Form, Button } from 'react-bootstrap'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Column from 'react-bootstrap/Column'
+import config from '../config'
 import Firebase from 'firebase'
+import ButtonIndicator from '../components/ButtonIndicator'
+import VarDisplay from '../components/VarDisplay'
+import VarIndicator from '../components/VarIndicator'
+import { DatabaseGet } from '../Database'
+import { DatabaseSet } from '../Database'
 
 const DoorOptions = () => {
 
@@ -9,38 +18,29 @@ const DoorOptions = () => {
 
     Firebase.app();
 
-    function getData() {
-		let ref = Firebase.database().ref('/TC/DriverEDoorCommand');
-		ref.on('value', snapshot => {
-			setEDoor(snapshot.val());
-		});
-        ref = Firebase.database().ref('/TC/DriverSDoorCommand');
-		ref.on('value', snapshot => {
-			setSDoor(snapshot.val());
-		});
-	}
+    useEffect(() => {setTimeout(() => DatabaseGet(setEDoor, 'Emergency Door', parentName), 500);}, [parentName]);
+    useEffect(() => {setTimeout(() => DatabaseGet(setSDoor, 'Service Door', parentName), 500);}, [parentName]);
 
-	function setData(newState, doorType) {
-        if(doorType == "e"){
-            Firebase.database().ref('/TC/DriverEDoorCommand').set(newState);
-        }
-        else if(doorType == "s"){
-            Firebase.database().ref('/TC/DriverSDoorCommand').set(newState);
-        }
-	}
-
-    useEffect(getData, []);
+    
 
     return (
-        <div>
-            <Container>
-                <Col>
-                    <ButtonIndicator parentName={eDoor} varName='eDoor' message='Emergency Door'/>
-                </Col>
-                <Col>
-                    <ButtonIndicator parentName={sDoor} varName='sDoor' message='Service Door'/>
-                </Col>
-            </Container>
+        <div style={{
+            textAlign: 'center',
+            background: '#cfdfe3',
+            width: '70%'
+        }}>
+            <h1>Door Options</h1>
+            <div style={{
+                textAlign: 'left',
+                paddingLeft: 50,
+                paddingRight: 50,
+                paddingBottom: 50
+            }}>
+                <Container fluid>
+                    <Row></Row>
+                </Container>
+            </div>
+            
         </div>
     )
 }
