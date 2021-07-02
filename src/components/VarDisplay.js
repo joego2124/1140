a647d70429f869d96faf69b4b626a38f6a7e2b46
Lucio <Lucio>
2021-07-02 16:18:14 -0400

    final fixes

--- src/components/VarDisplay.js
+++ src/components/VarDisplay.js
@@ -1,26 +1,21 @@
 import React, { useEffect, useState, useContext } from 'react';
-import { Button, Container, Row  } from 'react-bootstrap';
-import {DatabaseGet} from '../Database';
+import { Button, Container, Row } from 'react-bootstrap';
+import { DatabaseGet } from '../Database';
 
-function VarDisplay({varName, message, parentName}) {
-	
-	const [vari, setVari] = useState('default');
+function VarDisplay({ varName, message, parentName }) {
+  const [vari, setVari] = useState('default');
 
-	useEffect(() => {setTimeout(()=>DatabaseGet(setVari, varName, parentName), 500);}, [parentName]);
+  useEffect(() => {
+    setTimeout(() => DatabaseGet(setVari, varName, parentName), 500);
+  }, [parentName]);
 
-	return (
-		<div>
-<<<<<<< HEAD
-            <Container>
-                <Row>
-                    <h3>{message}: {vari}</h3>
-                </Row>
-            </Container>
-=======
-            <p>{message}: {vari}</p>
->>>>>>> benBranch
-		</div>
-	)
+  return (
+    <div>
+      <p>
+        {message}: {vari}
+      </p>
+    </div>
+  );
 }
 
-export default VarDisplay
+export default VarDisplay;
