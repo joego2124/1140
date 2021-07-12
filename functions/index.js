98ff5b278b72594db4356a1b1196b2d354620326
Calcu-dev <68208312+Calcu-dev@users.noreply.github.com>
2021-07-12 00:50:50 -0400

    Update function
    (NON-WORKING CODE)

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,5 @@
 const functions = require("firebase-functions");
+const admin = require('firebase-admin');
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -11,9 +12,31 @@ const functions = require("firebase-functions");
 exports.firstFunction = functions.https.onRequest(async (req, res) => {
     const original = req.query.text;
 
-    var b = eval('if (1) {10} else {20}');
+    var array = [];
+    var f1 = "(function hello(a,b) { return a+b;})";
+    var f2 = "(function goodbye(a,b) { return a-b;})";
 
-    
+    var num1 = 1;
+    var num2 = 2;
 
-    res.json({result: b});
+    array.push(eval(f1));
+    array.push(eval(f2));
+
+    var ref = admin.database().ref("test");
+    var childRef = ref.push();
+
+    while(true)
+    {
+        var c = 0;
+        array.forEach(e => c += e(num1,num2));
+
+        childRef.set({
+            title: "dbWrite",
+            value: c
+        });
+        num1 += 2;
+        num2 += 5;
+    }
+
+    res.json({result: c});
 })
