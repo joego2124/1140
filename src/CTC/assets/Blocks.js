404d1d3ee9d75a64ab7fa83f0ab8d8ce5f3a0dce
Joseph Gao <joegocod3@gmail.com>
2021-07-12 19:46:28 -0400

    track generation basic

---
+++ src/CTC/assets/Blocks.js
@@ -0,0 +1,9 @@
+export default {
+    "0011": <path fill-rule="evenodd" clip-rule="evenodd" d="M40 -6.55671e-07L55 0L55 4.01347L55 10L40 10L35 10L30 10C18.9543 10 10 18.9543 10 30L10 34L10 40L9.99999 55L4.01346 55L-6.21882e-06 55L-1.74846e-06 40L-1.31134e-06 30C-5.87108e-07 13.4315 13.4315 -1.81702e-06 30 -1.09278e-06L40 -6.55671e-07Z" fill="current"/>,
+    "0101": <path d="M5 100L5 9.53674e-07" stroke="current" stroke-width="10" stroke-linejoin="round"/>,
+    "0110": <path fill-rule="evenodd" clip-rule="evenodd" d="M40 55L55 55L55 50.9865L55 45L40 45L35 45L30 45C18.9543 45 10 36.0457 10 25L10 21L10 15L9.99999 1.96701e-06L4.01346 2.22869e-06L-6.21882e-06 2.40413e-06L-1.74846e-06 15L-1.31134e-06 25C-5.87108e-07 41.5685 13.4315 55 30 55L40 55Z" fill="current"/>,
+    "1001": <path fill-rule="evenodd" clip-rule="evenodd" d="M15 -6.55671e-07L0 0L1.75434e-07 4.01347L4.37114e-07 10L15 10L20 10L25 10C36.0457 10 45 18.9543 45 30L45 34L45 40L45 55L50.9865 55L55 55L55 40L55 30C55 13.4315 41.5685 -1.81702e-06 25 -1.09278e-06L15 -6.55671e-07Z" fill="current"/>,
+    "1010": <path d="M0 5H100" stroke="current" stroke-width="10" stroke-linejoin="round"/>,
+    "1100": <path fill-rule="evenodd" clip-rule="evenodd" d="M55 15L55 0L50.9865 -3.50868e-07L45 -8.74228e-07L45 15L45 20L45 25C45 36.0457 36.0457 45 25 45L21 45L15 45L-3.93403e-06 45L-4.45738e-06 50.9865L-4.80825e-06 55L15 55L25 55C41.5685 55 55 41.5685 55 25L55 15Z" fill="current"/>,
+    "circle": <path d="M35 65C51.5685 65 65 51.5685 65 35C65 18.4315 51.5685 5 35 5C18.4315 5 5 18.4315 5 35C5 51.5685 18.43015 65 35 65Z" fill="current" stroke="current" stroke-width="10"/>
+}
