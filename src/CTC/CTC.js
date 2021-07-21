c969e7adaf5f08903e0b4ff2a3c574e44362cba2
Joe <joegocod3@gmail.com>
2021-07-21 19:09:32 -0400

    track fixes

--- src/CTC/CTC.js
+++ src/CTC/CTC.js
@@ -10,47 +10,56 @@ import { DatabaseGet, DatabaseSet }  from "../Database";
 
 var trackLayout = require("./TrackLayout.json");
 
+//parallel breadth first search on bidirectional line respecting unidirectional train travel
 function bfs(startBlockId, endBlockId, _prevBlockId, lineLayout) {
 	console.warn("STARTING PATHFIND");
 
+	//define initial blocks
 	let startBlock = lineLayout.find(v => v.blockId === startBlockId);
 	let _prevBlock = lineLayout.find(v => v.blockId === _prevBlockId);
 	
-	let stack = [[{current: startBlock, previous: _prevBlock}]];
+	//define stack with starting block
+	let stack = [[{current: startBlock, previous: _prevBlock}]]; 
 
-	let counter = 0;
-	while (true) {
+	while (stack.length > 0) {
 
+		//get the current branch from stack
 		let currBranches = stack[stack.length - 1], newBranches = [];
-		console.log(stack[stack.length - 1]);
+		// console.log(stack[stack.length - 1]);
 
+		//evaluate each leaf in branch
 		for (let k = 0; k < currBranches.length; k++) {
 			let currentBlock = currBranches[k].current;
 			let prevBlock = currBranches[k].previous;
 
-			console.log(`currentBlock: ${currentBlock.blockId}, prevBlock: ${prevBlock.blockId}`);
+			// console.log(`currentBlock: ${currentBlock.blockId}, prevBlock: ${prevBlock.blockId}`);
 
+			//if the current leaf block is the destination
 			if (currentBlock.blockId == endBlockId) {
 				console.log("found solution");
-
 				let solution = [currentBlock.blockId];
-				let tempBlock = prevBlock;
+				let tempBlock = prevBlock; //temp block for reference
+
+				//reiterate backwards through all branches to root to get path traveled
 				for (let i = stack.length - 2; i > 0; i--) {
 					let currentLayer = stack[i];
 					let currentBlockLink = currentLayer.find(v => v.current == tempBlock);
 					solution.push(currentBlockLink.current.blockId);
 					tempBlock = currentBlockLink.previous;
 				}
-				console.log(solution);
 
-				return true;
+				return solution.reverse(); //return to caller function
 			}
 
+			//iterate through all possible connections in current leaf block
 			for (let i = 0; i < currentBlock.connectors.length; i++) {
 				for (let j = 0; j < currentBlock.connectors[i].length; j++) {
-					let neighborBlockId = currentBlock.connectors[i][j];
-					if (neighborBlockId != null && neighborBlockId != prevBlock.blockId) {
+					let neighborBlockId = currentBlock.connectors[i][j]; //neighboring block
+					//make sure block exists and isn't the previous block
+					if (neighborBlockId != null && neighborBlockId != prevBlock.blockId) { 
+						//make sure the move to neighboring block is valid
 						if (currentBlock.connectors[i].find(id => id === prevBlock.blockId) != undefined) {
+							//add move and push leaf to new branch
 							let neighborBlock = lineLayout.find(v => v.blockId === neighborBlockId);
 							newBranches.push({
 								current: neighborBlock,
@@ -63,38 +72,34 @@ function bfs(startBlockId, endBlockId, _prevBlockId, lineLayout) {
 		}
 
 		stack.push(newBranches);
-		counter++;
 	}
 	
-	console.warn("END PATHFIND");
+	console.warn(`END PATHFIND: NO SOLUTION FOUND FROM ${startBlockId} TO ${endBlockId} STARTING AT ${_prevBlockId}`);
 }
 
-// function routeTrain(stations) {
-// 	let yardId = trackLayout.blocks.find(v => v.section === "YARD").blockId;
-// 	let prevStationId = yardId, prevBlockId = yardId;
-// 	let route = [];
-
+function routeTrain(stations, lineColor, returnToYard) {
+	let yardId = trackLayout.blocks.find(v => v.section === "YARD").blockId;
+	let prevStationId = yardId;
+	let route = [];
+	let lineLayout = lineColor == "red" ? trackLayout.redLine : trackLayout.greenLine;
 	
+	// iterate through each station and find path
+	stations.forEach(nextStationId => {
+		let subroute = bfs(lineColor === "red" ? -1 : -1, nextStationId, lineColor === "red" ? -1.1 : -1.1, lineLayout);
+		route = route.concat(subroute);
+		prevStationId = nextStationId;
+	});
 
-// 	prevBlockId = 34;
-// 	console.log(pathfind(35, 34));
+	console.log("routing back to yard");
 
-	//iterate through each station and find path
-	// stations.sort((a, b) => a - b).forEach(nextStationId => {
-	// 	let subroute = pathfind(prevStationId, nextStationId);
-	// 	route = route.concat(subroute);
-	// 	prevStationId = nextStationId;
-	// });
-
-	// console.log("routing back to yard");
-
-	// //go back to yard
-	// prevBlockId = route[route.length - 1];
-	// route = route.concat(pathfind(prevStationId, yardId));
-	// route.push(yardId); //push yard block as final since pathfind does not
+	//go back to yard
+	if (returnToYard) {
+		route = route.concat(bfs(prevStationId, lineColor === "red" ? -1 : -1, route[route.length - 1], lineLayout));
+		route.push(yardId); //push yard block as final since pathfind does not
+	}
 	
-// 	return route;
-// }
+	return route;
+}
 
 function CTC() {
 
@@ -108,7 +113,7 @@ function CTC() {
 	}, []);
 
 	// console.log(routeTrain([35, 16]));
-	console.log(bfs(9, 60, -1, trackLayout.redLine));
+	console.log(bfs(9, 16, -1, trackLayout.redLine));
 
 	return (
 		<div>
