03ccb4edf98664ed1912de44fb2e8f482cfd9483
Lucio <Lucio>
2021-07-21 16:42:18 -0400

    more changes

--- src/WaysideController/WaysidePanel.js
+++ src/WaysideController/WaysidePanel.js
@@ -8,10 +8,11 @@ import 'react-sliding-pane/dist/react-sliding-pane.css';
 import './styles.css';
 import waysideControllers from './WaysideControllers.json';
 
-const WaysidePanel = () => {
+const WaysidePanel = ({ setterForSelectedWayside }) => {
   const [open, setOpen] = useState(true);
   const [blockList, setBlockList] = useState([]);
   const [waysideList, setWaysideList] = useState([]);
+  const [selectedWayside, setSelectedWayside] = useState([]);
 
   Firebase.app();
 
@@ -43,40 +44,54 @@ const WaysidePanel = () => {
   //   return console.log(list);
   // });
 
-  // function setWaysideListData(newState) {
-  // 	Firebase.database().ref('/trackController/Waysidelist').set(newState);
-  // }
+  const SetSelectedWaysideData = (newState) => {
+    setSelectedWayside(newState);
+    console.log(newState);
+    setterForSelectedWayside(newState);
+    // Firebase.database().ref('/WSC/SelectedWayside/').set(newState);
+  };
 
   useEffect(() => getBlockListData(), []);
 
-  function getWaysideList() {
+  function getWaysideListData() {
     let WSC1 = [56, 57, 57, 59, 60, 0];
-
     let WSC2 = [61, 62, 0, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73];
-    let tempWaysideList = [];
-    for (let i = 0; i < WSC1.length; i++) {
-      tempWaysideList.push(blockList[WSC1[i]]);
-      console.log(tempWaysideList);
+    let tempWaysideList = [WSC1, WSC2];
+
+    let tempIndividualWaysideBlockList = [];
+    let waysides = [];
+    for (let i = 0; i < tempWaysideList.length; i++) {
+      tempIndividualWaysideBlockList = [];
+      for (let j = 0; j < tempWaysideList[i].length; j++) {
+        tempIndividualWaysideBlockList.push(blockList[tempWaysideList[i][j]]);
+      }
+      console.log(tempIndividualWaysideBlockList);
+      waysides.push(tempIndividualWaysideBlockList);
     }
-    console.log(tempWaysideList);
-    setWaysideList(tempWaysideList);
-    console.log(waysideList);
+    setWaysideList(waysides);
   }
 
+  useEffect(() => getWaysideListData(), []);
+
   function ShowWaysideList() {
-    getWaysideList();
     console.log(waysideList);
     console.log(waysideList[0]);
-    const listItems = 0;
-    for (let i = 0; i < 2; i++) {
-      listItems += (
-        <Button variant='light' className='waysideButton' value={waysideList}>
+    let listItems = 0;
+    for (let i = 0; i < waysideList.length; i++) {
+      console.log(i);
+      listItems = waysideList.map((waysideIndex, i) => (
+        <Button
+          variant='light'
+          className='waysideButton'
+          onClick={SetSelectedWaysideData(waysideIndex)}
+          value={waysideIndex}
+        >
           <div className='buttonDiv'>
             <BsCircleFill size='1.5em' color='#C44242' />
-            <div className='buttonText'>{waysideList[0].BlockNumber}</div>
+            <div className='buttonText'>WSC{i + 1}</div>
           </div>
         </Button>
-      );
+      ));
     }
     return <div className='waysidePanelHolder'>{listItems}</div>;
   }
