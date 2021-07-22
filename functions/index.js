be4dd75505d7c5caa60acc296fd609879497bf90
Lucio <Lucio>
2021-07-22 17:41:43 -0400

    fix index

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,4 @@
-const functions = require("firebase-functions");
+const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 
 //import from individual files
@@ -10,13 +10,9 @@ exports.onEDoorUpdate = trainModel.onEDoorUpdate;
 // exports.physicsTick = trainModel.physicsTick;
 exports.changeTrainLength = trainModel.changeTrainLength;
 exports.changePassengers = trainModel.changePassengers;
-<<<<<<< HEAD
 // exports.trainConsoleTest = trainModel.trainConsoleTest;
 
 exports.waysideTick = wayside.runLogic;
-
-=======
-<<<<<<< HEAD
 exports.onBrakeFailure = trainControllerDriver.onBrakeFailure;
 exports.onEngineFailure = trainControllerDriver.onEngineFailure;
 exports.onSignalFailure = trainControllerDriver.onSignalFailure;
@@ -24,10 +20,6 @@ exports.onIntTempChange = trainControllerDriver.onIntTempChange;
 exports.onDoorOpen = trainControllerDriver.onDoorOpen;
 exports.onSetpointSpeed = trainControllerDriver.onSetpointSpeed;
 
-=======
 exports.trainAdded = trainModel.trainAdded;
 exports.trainRemoved = trainModel.trainRemoved;
->>>>>>> 5456021fd7c56adc0a6beaa4774495907b5c49d6
 // exports.trainConsoleTest = trainModel.trainConsoleTest;
-
->>>>>>> 7ebf5c9aa28e20ec40995a3bf1556ecc5087de80
