9a47c5656a4f3150381473bdcb26d2a780e8da4c
Bentanay <55263304+Bentanay@users.noreply.github.com>
2021-07-27 20:54:38 -0400

    committing to please the git gods

--- src/TrainController/TemperatureOptions.js
+++ src/TrainController/TemperatureOptions.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Button, Container, Col, Row  } from 'react-bootstrap';
 import VarDisplay from '../components/VarDisplay';
 import VarInput from '../components/VarInput';
+import TempInput from '../components/TempInput';
 import VarIndicator from '../components/VarIndicator';
 import ButtonIndicator from '../components/ButtonIndicator';
 
@@ -13,7 +14,7 @@ function TemperatureOptions(parentName, selectedTrain) {
                 <Col>
                     <VarDisplay selectedTrain={selectedTrain} parentName={parentName} varName='ExternalTemperature' message='Ext. Temp' units='°F'/>
                     <VarDisplay selectedTrain={selectedTrain} parentName={parentName} varName='InternalTemperature' message='Int. Temp' units='°F'/>
-                    <TempInput  selectedTrain={selectedTrain} parentName={parentName} varName='InternalTemperature' message='Int. Temp'/>
+                    <TempInput selectedTrain={selectedTrain} parentName={parentName} varName='InternalTemperature' message='Int. Temp'/>
                 </Col>
             </Container>
         </div>
