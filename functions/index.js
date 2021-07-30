22b46eccc63d03d61591235be08863f290a051e2
VANA01 <NPV1@pitt.edu>
2021-07-30 17:53:08 -0400

    Fixed borked

--- functions/index.js
+++ functions/index.js
@@ -10,13 +10,10 @@ var trackLayout = require("./TrackLayout.json");
 
 //imports from train model
 exports.onEDoorUpdate = trainModel.onEDoorUpdate;
-// exports.physicsTick = trainModel.physicsTick;
 exports.changeTrainLength = trainModel.changeTrainLength;
 exports.changePassengers = trainModel.changePassengers;
 exports.trainAdded = trainModel.trainAdded;
 exports.trainRemoved = trainModel.trainRemoved;
-
-// exports.trainConsoleTest = trainModel.trainConsoleTest;
 
 exports.waysideTick = wayside.runLogic;
 exports.onBrakeFailure = trainControllerDriver.onBrakeFailure;
@@ -26,7 +23,7 @@ exports.onIntTempChange = trainControllerDriver.onIntTempChange;
 exports.onDoorOpen = trainControllerDriver.onDoorOpen;
 exports.onSetpointSpeed = trainControllerDriver.onSetpointSpeed;
 
-// exports.trainConsoleTest = trainModel.trainConsoleTest;
+
 exports.onCTCTrainCurrentBlockChange = functions.database.ref('/TrainList/{trainId}/CurrentBlock').onUpdate((change, context) => {
   const database = admin.database();
 
