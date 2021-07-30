399f426232ad7d0994875eb2f0e8785b304ce69a
VANA01 <NPV1@pitt.edu>
2021-07-30 15:33:36 -0400

    temp nathan

--- functions/index.js
+++ functions/index.js
@@ -10,17 +10,19 @@ exports.onEDoorUpdate = trainModel.onEDoorUpdate;
 // exports.physicsTick = trainModel.physicsTick;
 exports.changeTrainLength = trainModel.changeTrainLength;
 exports.changePassengers = trainModel.changePassengers;
-<<<<<<< HEAD
+exports.trainAdded = trainModel.trainAdded;
+exports.trainRemoved = trainModel.trainRemoved;
+
 exports.onBrakeFailure = trainControllerDriver.onBrakeFailure;
 exports.onEngineFailure = trainControllerDriver.onEngineFailure;
 exports.onSignalFailure = trainControllerDriver.onSignalFailure;
 exports.onIntTempChange = trainControllerDriver.onIntTempChange;
 exports.onDoorOpen = trainControllerDriver.onDoorOpen;
 exports.onSetpointSpeed = trainControllerDriver.onSetpointSpeed;
+exports.onSpeedLimitChange = trainControllerDriver.onSpeedLimitChange;
+exports.onAuthorityChange = trainControllerDriver.onAuthorityChange;
+exports.onAuthorityChange = trainControllerDriver.onSetpointSpeedChange;
 
-=======
-exports.trainAdded = trainModel.trainAdded;
-exports.trainRemoved = trainModel.trainRemoved;
->>>>>>> 5456021fd7c56adc0a6beaa4774495907b5c49d6
+
 // exports.trainConsoleTest = trainModel.trainConsoleTest;
 
