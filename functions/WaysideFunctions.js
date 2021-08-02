1fbfec2486ac4140230264b3b5c1699b28e0fe74
Calcu-dev <68208312+Calcu-dev@users.noreply.github.com>
2021-08-01 23:54:05 -0400

    Update wayside function to target all waysides

--- functions/WaysideFunctions.js
+++ functions/WaysideFunctions.js
@@ -7,11 +7,16 @@ exports.runLogic = functions.database.ref('/SimulationClock/Time').onUpdate((cha
     const database = admin.database();
 
     //if only one promise is generated we can return it alone. If no asyncronous operations are used we don't have to return a promise, and can return null instead
-    promises.push(database.ref('/WSC/WSC1/Script').get().then((snapshot => {
-        const script = snapshot.val();
-        console.log(script);
+    promises.push(database.ref('/WSC/').get().then((snapshot => {
+        const waysides = snapshot.val();
 
-        eval(script);
+        for (wayside in waysides){
+            if(wayside.includes('WSC'))
+            {
+                var script = wayside.Script;
+                eval(script);
+            }
+        }
     })))
    
     return Promise.all(promises);
