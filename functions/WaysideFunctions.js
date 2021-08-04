937dbcc4b83e717bc8d3b2f4c07b2595e2fbd3a9
Calcu-dev <68208312+Calcu-dev@users.noreply.github.com>
2021-08-03 21:12:20 -0400

    Update Wayside controller to actually run waysides...

--- functions/WaysideFunctions.js
+++ functions/WaysideFunctions.js
@@ -9,12 +9,11 @@ exports.runLogic = functions.database.ref('/SimulationClock/Time').onUpdate((cha
     //if only one promise is generated we can return it alone. If no asyncronous operations are used we don't have to return a promise, and can return null instead
     promises.push(database.ref('/WSC/').get().then((snapshot => {
         const waysides = snapshot.val();
-        console.log("[WaysideFunctions] running...");
 
         for (wayside in waysides){
             if(wayside.includes('WSC'))
             {
-                var script = wayside.Script;
+                var script = waysides[wayside].Script;
                 eval(script);
             }
         }
